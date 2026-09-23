8818c686265cacbaa0f9e29a9dfad87b194fba0f
alejacos <aleacosta969@gmail.com>
2026-09-22 21:25:32 -0300

    Ya verifica los campos con un 80% de aciertos y genera el csv con los resultados. Resta detectar los alumnos que no hayan aprobado (L o R) y guardar los crops con el resto de la info solicitada.

--- TP1/src/problema_2_preliminar.py
+++ TP1/src/problema_2_preliminar.py
@@ -1,8 +1,20 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def buscar_letras(crop_celda, umbral=170, area_minima=2):
+    """
+    Función para detectar caracteres en una celda de una hoja de calificaciones.
+    Parámetros:
+    - crop_celda: imagen recortada de la celda
+    - umbral: umbral para la binarización
+    - area_minima: área mínima para considerar un componente como un carácter
+    Retorna:
+    - caracteres: lista de diccionarios con información de cada carácter detectado
+    - cantidad: número de caracteres detectados
+    """
+
     # 1. Convertir a escala de grises si es necesario
     if len(crop_celda.shape) == 3:
         crop = cv2.cvtColor(crop_celda, cv2.COLOR_BGR2GRAY)
@@ -33,10 +45,19 @@ def buscar_letras(crop_celda, umbral=170, area_minima=2):
 
     
 def contar_palabras(caracteres, umbral_espacio=6):
-    # Umbral empírico: un espacio mayor a 12 píxeles suele indicar separación de palabras
+    """
+    Función para contar palabras en una lista de caracteres detectados.
+    Parámetros:
+    - caracteres: lista de diccionarios con información de cada carácter detectado
+    - umbral_espacio: umbral para considerar un espacio como separador de palabras
+    Retorna:
+    - cantidad_palabras: número de palabras detectadas
+    """
+
+    # Umbral empírico: un espacio mayor a 6 píxeles suele indicar separación de palabras
     # Si la celda está vacía, no hay palabras
     if not caracteres:
-        print("Cantidad de palabras detectadas: 0 (Celda vacía)")
+        #print("Cantidad de palabras detectadas: 0 (Celda vacía)")
         return 0
 
     # --- ORDENAR LOS CARACTERES DE IZQUIERDA A DERECHA ---
@@ -71,8 +92,8 @@ def contar_palabras(caracteres, umbral_espacio=6):
 #Comienzo del código para procesar las hojas de calificaciones
 #========================================================================================
 
-#hojas = ["1", "2", "3", "4"]
-hojas = ["4"]
+hojas = ["1", "2", "3", "4"]
+registros = []
 
 #Itero sobre cada hoja de calificaciones
 for hoja in hojas:
@@ -93,8 +114,9 @@ for hoja in hojas:
     alto_columna = linea_horizontal[-1] - linea_horizontal[1]
     (linea_vertical, ) = np.where(img_cols > alto_columna)
 
-    # Itero sobre cada línea horizontal para extraer las celdas correspondientes a Legajo, Nombre, Parcial 1,
+    # Itero sobre cada línea horizontal de cada hoja para extraer las celdas correspondientes a Legajo, Nombre, Parcial 1,
     # Parcial 2, Parcial 3 y Condición Final
+    print(f"Procesando hoja {hoja}...")
     for i in range(1, len(linea_horizontal) - 1):
         linea_superior = linea_horizontal[i] + 2
         linea_inferior = linea_horizontal[i + 1] - 2
@@ -105,27 +127,28 @@ for hoja in hojas:
         crop_parcial_3 = img[linea_superior:linea_inferior, linea_vertical[5]+2:linea_vertical[6]-2]
         crop_condicion_final = img[linea_superior:linea_inferior, linea_vertical[6]+2:linea_vertical[7]-2]
 
-        """
+        print(f"Registro {i}:")
         #Verificar campo Legajo ==============================================================
-        caracteres, cantidad = buscar_letras(crop_legajo, umbral=138, area_minima=2)
-
-        print(f"Legajo {i}: {cantidad} letras detectadas.")
-        plt.imshow(crop_legajo, cmap="gray")
-        plt.title(f"Hoja {hoja} - Legajo {i}: {cantidad} letras detectadas.")
-        plt.show()
+        _, cantidad_legajo = buscar_letras(crop_legajo, umbral=138, area_minima=2)
+        if cantidad_legajo != 8:
+            campo_legajo = "MAL"
+        else:
+            campo_legajo = "OK"
+        print(f"Legajo: {campo_legajo}")
         
-        
-        #Verificar campo Nombre ==============================================================
+        #Verificar campo Nombre y apellido ======================================================
         caracteres_nombre, cantidad_letras = buscar_letras(
             crop_nombre, umbral=138, area_minima=6
         )
         cantidad_palabras = contar_palabras(caracteres_nombre, umbral_espacio=2)
 
-        print(f"Legajo {i}: {cantidad_palabras} palabras detectadas.")
-        plt.imshow(crop_nombre, cmap="gray")
-        plt.title(f"Hoja {hoja} - Nombre {i}: {cantidad_letras} letras detectadas y {cantidad_palabras} palabras detectadas.")
-        plt.show()
-        
+        #Debe contener un minimo de 2 palabras (Apellido y Nombre) y no más de 12 caracteres
+        if cantidad_palabras < 2 or cantidad_letras > 12:
+            campo_nombre = "MAL"
+        else:
+            campo_nombre = "OK"
+        print(f"Nombre: {campo_nombre}")
+      
         
         #Verificar campo Parcial_1 ==============================================================
         caracteres_parcial_1, cantidad_letras = buscar_letras(
@@ -133,48 +156,84 @@ for hoja in hojas:
         )
         cantidad_palabras = contar_palabras(caracteres_parcial_1, umbral_espacio=2)
 
-        print(f"Legajo {i}: {cantidad_palabras} palabras detectadas.")
-        plt.imshow(crop_parcial_1, cmap="gray")
-        plt.title(f"Hoja {hoja} - Parcial 1 Linea Nro: {i}: {cantidad_letras} letras detectadas.")
-        plt.show()
-        
-        
+        # Debe contener 1 o dos caracteres consecutivos (nota del parcial)
+        if cantidad_letras < 1 or cantidad_letras > 2:
+            campo_parcial_1 = "MAL"
+        else:
+            campo_parcial_1 = "OK"
+        print(f"Parcial 1: {campo_parcial_1}")
+
         #Verificar campo Parcial_2 ==============================================================
         caracteres_parcial_2, cantidad_letras = buscar_letras(
             crop_parcial_2, umbral=138, area_minima=9
         )
         cantidad_palabras = contar_palabras(caracteres_parcial_2, umbral_espacio=2)
 
-        print(f"Legajo {i}: {cantidad_palabras} palabras detectadas.")
-        plt.imshow(crop_parcial_2, cmap="gray")
-        plt.title(f"Hoja {hoja} - Parcial 2 Linea Nro: {i}: {cantidad_letras} letras detectadas.")
-        plt.show()
-        
-        
+        # Debe contener 1 o dos caracteres consecutivos (nota del parcial)
+        if cantidad_letras < 1 or cantidad_letras > 2:
+            campo_parcial_2 = "MAL"
+        else:
+            campo_parcial_2 = "OK"
+        print(f"Parcial 2: {campo_parcial_2}")
+      
         #Verificar campo Parcial_3 ==============================================================
         caracteres_parcial_3, cantidad_letras = buscar_letras(
             crop_parcial_3, umbral=138, area_minima=9
         )
         cantidad_palabras = contar_palabras(caracteres_parcial_3, umbral_espacio=2)
 
-        print(f"Legajo {i}: {cantidad_palabras} palabras detectadas.")
-        plt.imshow(crop_parcial_3, cmap="gray")
-        plt.title(f"Hoja {hoja} - Parcial 3 Linea Nro: {i}: {cantidad_letras} letras detectadas.")
-        plt.show()
+        # Debe contener 1 o dos caracteres consecutivos (nota del parcial)
+        if cantidad_letras < 1 or cantidad_letras > 2:
+            campo_parcial_3 = "MAL"
+        else:
+            campo_parcial_3 = "OK"
+        print(f"Parcial 3: {campo_parcial_3}")
 
-        
         #Verificar campo condicion final ==============================================================
         caracteres_condicion_final, cantidad_letras = buscar_letras(
             crop_condicion_final, umbral=138, area_minima=9
         )
         cantidad_palabras = contar_palabras(caracteres_condicion_final, umbral_espacio=2)
 
-        print(f"Legajo {i}: {cantidad_palabras} palabras detectadas.")
-        plt.imshow(crop_condicion_final, cmap="gray")
-        plt.title(f"Hoja {hoja} - Condición Final Linea Nro: {i}: {cantidad_letras} letras detectadas.")
-        plt.show()
+        # Debe contener 1 unica letra (A, R, L)
+        if cantidad_letras != 1:
+            campo_condicion_final = "MAL"
+        else:
+            campo_condicion_final = "OK"
+        print(f"Condición Final: {campo_condicion_final}")
 
-        """
+        registros.append({
+            "Hoja": hoja,
+            "Id": i,
+            "Legajo": campo_legajo,
+            "Nombre_apellido": campo_nombre,
+            "Parcial_1": campo_parcial_1,
+            "Parcial_2": campo_parcial_2,
+            "Parcial_3": campo_parcial_3,
+            "Condicion_final": campo_condicion_final,
+        })
+
+
+df_resultados = pd.DataFrame(
+    registros,
+    columns=[
+        "Hoja",
+        "Id",
+        "Legajo",
+        "Nombre_apellido",
+        "Parcial_1",
+        "Parcial_2",
+        "Parcial_3",
+        "Condicion_final",
+    ],
+)
+df_resultados.to_csv(
+    "./git/TUIA_PDI_TP1_2026_C2_outl/TP1/outputs/validacion_resultados.csv",
+    index=False,
+)
+print("Resultados guardados en validacion_resultados.csv")
+
+        
 
 """
 def main():
