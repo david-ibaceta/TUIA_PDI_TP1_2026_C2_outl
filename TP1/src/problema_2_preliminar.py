d943b5acbab8da04867cf0f8125ced7b9e0fd044
alejacos <aleacosta969@gmail.com>
2026-09-24 17:25:49 -0300

    Agrego segmentacion por ancho de letra y mejora la deteccion

--- TP1/src/problema_2_preliminar.py
+++ TP1/src/problema_2_preliminar.py
@@ -1,8 +1,22 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-def buscar_letras(crop_celda, umbral=170, area_minima=2):
+def buscar_letras(crop_celda, umbral=170, area_minima=2, ANCHO_PROMEDIO_LETRA = 10, UMBRAL_LETRA_UNICA = 14):
+    """
+    Función para detectar caracteres en una celda de una hoja de calificaciones.
+    Parámetros:
+    - crop_celda: imagen recortada de la celda
+    - umbral: umbral para la binarización
+    - area_minima: área mínima para considerar un componente como un carácter
+    - ANCHO_PROMEDIO_LETRA: Ancho estimado de una letra + su espacio
+    - UMBRAL_LETRA_UNICA: Si mide menos de esto, es 1 sola letra de forma segura
+    Retorna:
+    - caracteres: lista de diccionarios con información de cada carácter detectado
+    - total_caracteres: número de caracteres detectados
+    """
+
     # 1. Convertir a escala de grises si es necesario
     if len(crop_celda.shape) == 3:
         crop = cv2.cvtColor(crop_celda, cv2.COLOR_BGR2GRAY)
@@ -17,19 +31,41 @@ def buscar_letras(crop_celda, umbral=170, area_minima=2):
 
     # 4. Filtrar por área mínima    
     # Empezamos en 1 para omitir el fondo (label 0)
+    # Parámetros de calibración
+
     caracteres = []
-    cantidad = 0
+    total_caracteres = 0
     for i in range(1, num_labels):
         area = stats[i, cv2.CC_STAT_AREA]
-        if area >= area_minima:
-            cantidad += 1
-            caracteres.append({
-                "componente": i,
-                "bbox_xywh": (stats[i, 0], stats[i, 1], stats[i, 2], stats[i, 3]), # x, y, w, h
-                "area": area,
-                "centroide": centroids[i] #(cx,cy)
-            })
-    return caracteres, cantidad
+        if area < area_minima:
+            continue
+        x = stats[i, 0]
+        y = stats[i, 1]
+        w = stats[i, 2]
+        h = stats[i, 3]
+        caracteres.append({
+            "componente": i,
+            "bbox_xywh": (w, y, w, h),
+            "area": area,
+            "centroide": centroids[i] #(cx,cy)
+        })
+            
+        # --- ESTRATEGIA DE CONTEO ESTIMADO ---
+        if w <= UMBRAL_LETRA_UNICA:
+            # Caso normal: el componente entra en el ancho de una sola letra
+            caracteres_en_componente = 1
+        else:
+            # Caso caracteres pegados: calculamos cuántas letras entran en ese ancho.
+            # Usamos división entera sumando un redondeo dinámico para casos límite.
+            caracteres_en_componente = int(np.round(w / ANCHO_PROMEDIO_LETRA))
+            
+            # Aseguramos que al menos compute 2 si superó el umbral superior
+            if caracteres_en_componente < 2:
+                caracteres_en_componente = 2
+                
+        total_caracteres += caracteres_en_componente
+        
+    return caracteres, total_caracteres
 
     
 def contar_palabras(caracteres, umbral_espacio=6):
@@ -81,12 +117,21 @@ for hoja in hojas:
         cv2.IMREAD_GRAYSCALE
     )
 
+    # Aplicamos cv2.GaussianBlur para limpiar ruido
+    # Usamos un kernel pequeño (5x5) y dejamos que calcule sigma automáticamente (0)
+    img_suavizada = cv2.GaussianBlur(img, (5, 5), 0)
+   
+    # Binarizamos la imagen para detectar las líneas oscuras de la grilla
+    img_binaria = img < 128
+
+    # Binarizamos la imagen (Fondo Blanco 255, Letra Negra 0)
+    # Usamos el método de Otsu para que se adapte a la luz de la hoja
+    #_, img_binaria = cv2.threshold(img_suavizada, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+
     # Determino la posición de las líneas horizontales y verticales para segmentar la hoja en celdas
-    # Umbral para binarizar la imagen y detectar líneas
-    th = 10
-    img_th = img < th
-    img_rows = np.sum(img_th, axis=1)
-    img_cols = np.sum(img_th, axis=0)
+    img_rows = np.sum(img_binaria, axis=1)
+    img_cols = np.sum(img_binaria, axis=0)
 
     # Identifico las posiciones de las líneas horizontales y verticales
     (linea_horizontal, ) = np.where(img_rows >= (img_rows.max() - 2))
@@ -95,7 +140,7 @@ for hoja in hojas:
 
     # Itero sobre cada línea horizontal para extraer las celdas correspondientes a Legajo, Nombre, Parcial 1,
     # Parcial 2, Parcial 3 y Condición Final
-    for i in range(1, len(linea_horizontal) - 1):
+    for i in range(1, 8): #len(linea_horizontal) - 1):
         linea_superior = linea_horizontal[i] + 2
         linea_inferior = linea_horizontal[i + 1] - 2
         crop_legajo = img[linea_superior:linea_inferior, linea_vertical[1]+2:linea_vertical[2]-2]
@@ -105,16 +150,16 @@ for hoja in hojas:
         crop_parcial_3 = img[linea_superior:linea_inferior, linea_vertical[5]+2:linea_vertical[6]-2]
         crop_condicion_final = img[linea_superior:linea_inferior, linea_vertical[6]+2:linea_vertical[7]-2]
 
-        """
+        
         #Verificar campo Legajo ==============================================================
-        caracteres, cantidad = buscar_letras(crop_legajo, umbral=138, area_minima=2)
+        caracteres, cantidad = buscar_letras(crop_legajo, umbral=138, area_minima=3)
 
         print(f"Legajo {i}: {cantidad} letras detectadas.")
         plt.imshow(crop_legajo, cmap="gray")
         plt.title(f"Hoja {hoja} - Legajo {i}: {cantidad} letras detectadas.")
         plt.show()
         
-        
+        """
         #Verificar campo Nombre ==============================================================
         caracteres_nombre, cantidad_letras = buscar_letras(
             crop_nombre, umbral=138, area_minima=6
